2d69065c1eae8cba52affe14a0e3b09b4afde32a
Ritik-1118 <ritik11181@gmail.com>
2023-02-06 17:52:56 +0530

    static and templates files added

--- tec_spot/urls.py
+++ tec_spot/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('login',views.login, name='index'),
+    path('about',views.about, name='about'),
+    path('blog',views.blog, name='blog'),
+    path('contact',views.contact, name='contact'),
+    path('detail',views.detail, name='detail'),
+    path('feature',views.feature, name='feature'),
+    path('quote',views.quote, name='quote'),
+    path('service',views.service, name='service'),
+    path('team',views.team, name='team'),
+    path('testimonial',views.testimonial, name='testimonial'),
 ]
--- tec_spot/views.py
+++ tec_spot/views.py
@@ -4,3 +4,39 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def detail(request):
+    return render(request, 'detail.html')
+
+
+def feature(request):
+    return render(request, 'feature.html')
+
+
+def quote(request):
+    return render(request, 'quote.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
